6f3d676d42be4af1a49be0974b5d0559269a8e12
Daniel Norton <421735+danielnorton@users.noreply.github.com>
2020-01-28 16:32:54 -0600

    exercise 1

--- src/week1/exercise1/mod.rs
+++ src/week1/exercise1/mod.rs
@@ -1,3 +1,11 @@
-pub fn hello_world() -> i64 {
-    0
+pub fn parity(source: &Vec<i64>) -> Vec<i64> {
+    
+    // Given an array A of non-negative integers, return an array 
+    // consisting of all the even elements of A, followed by all 
+    // the odd elements of A.
+
+    let even: Vec<i64> = source.iter().cloned().filter(|x| *x % 2 == 0).collect();
+    let odd: Vec<i64> = source.iter().cloned().filter(|x| *x % 2 == 1).collect();
+
+    return [&even[..], &odd[..]].concat();
 }
--- tests/week1/exercise1/mod.rs
+++ tests/week1/exercise1/mod.rs
@@ -1,6 +1,25 @@
-use onboarding_rust::week1::exercise1::hello_world;
+use onboarding_rust::week1::exercise1::parity;
 
 #[test]
-fn test_week1_exercise1() {
-    assert_eq!(0, hello_world());
+fn test_week1_exercise1_001() {
+
+    let v1: Vec<i64> = vec![1,2,3,4,5,6,7,8,9];
+    let t1: Vec<i64> = vec![2,4,6,8,1,3,5,7,9];
+    assert_eq!(t1, parity(&v1).clone());
+}
+
+#[test]
+fn test_week1_exercise1_002() {
+
+    let v1: Vec<i64> = vec![5,7,3,1,5,8,9,6,45,3];
+    let t1: Vec<i64> = vec![8,6,5,7,3,1,5,9,45,3];
+    assert_eq!(t1, parity(&v1).clone());
+}
+
+#[test]
+fn test_week1_exercise1_003() {
+
+    let v1: Vec<i64> = vec![];
+    let t1: Vec<i64> = vec![];
+    assert_eq!(t1, parity(&v1).clone());
 }
